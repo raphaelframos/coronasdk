021883ddab4f02e3696039fbd1afe565db976eb1
Raphael <raphaelframos@gmail.com>
2020-01-01 23:12:18 -0300

    Criando o chao, com fisica estatica

--- Fisica1/main.lua
+++ Fisica1/main.lua
@@ -6,4 +6,8 @@ background:setFillColor( 1, 1, 1 )
 local player = display.newCircle( display.contentCenterX, 10, 15 )
 player:setFillColor( 0, 0, 0 )
 
+local floor = display.newRect( display.contentCenterX, display.actualContentHeight-50, display.actualContentWidth, 10 )
+floor:setFillColor( 0, 1, 0 )
+
 physics.addBody( player )
+physics.addBody( floor, "static" )
