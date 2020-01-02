3a41dc0df1d9171f16650d563de2088c6f5f603b
Raphael <raphaelframos@gmail.com>
2020-01-01 23:09:47 -0300

    Importando e iniciando fisica no player

--- Fisica1/main.lua
+++ Fisica1/main.lua
@@ -1,5 +1,9 @@
+local physics = require "physics"
+physics.start()
 local background = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
 background:setFillColor( 1, 1, 1 )
 
 local player = display.newCircle( display.contentCenterX, 10, 15 )
 player:setFillColor( 0, 0, 0 )
+
+physics.addBody( player )
