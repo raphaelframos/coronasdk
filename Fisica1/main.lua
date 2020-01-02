8ec322af5704bf566643643950f268ae0f885c4c
Raphael <raphaelframos@gmail.com>
2020-01-01 23:08:22 -0300

    Criando um jogador como um circulo

--- Fisica1/main.lua
+++ Fisica1/main.lua
@@ -1,2 +1,5 @@
 local background = display.newRect( display.contentCenterX, display.contentCenterY, display.actualContentWidth, display.actualContentHeight )
-background:setFillColor( 1, 1, 1)
+background:setFillColor( 1, 1, 1 )
+
+local player = display.newCircle( display.contentCenterX, 10, 15 )
+player:setFillColor( 0, 0, 0 )
